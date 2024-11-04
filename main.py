1d37aa926ca1800dfbfedbedc2a3fc918599f8e0
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2024-11-03 22:29:03 -0300

    Added more context

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
-model = OllamaLLM(model='llama3.2:1b')
-result = model.invoke(input='Como você está?')
-print(result)
+template = """
+Answer the following question:
+
+The context: {context}
+
+The question: {question}
+
+Answer:
+"""
+
+model = OllamaLLM(model="llama3.2:1b")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def conversation():
+    context = """
+    You're a salesman from a clothes store called "My precious" 
+    Today is Black Friday and all clothes are 30% off.
+    In your store they sell only t-shirts.
+    """
+
+    print("Welcome to our store, how may I help you? Type 'quit' to leave our store.")
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == "quit":
+            break
+
+        result = chain.invoke({"context": context, "question": user_input})
+        print(f"> Salesman: {result}")
+        context += f"\nCostumer: {user_input}\nSalesman: {result}"
+
+if __name__ == '__main__':
+    conversation()
